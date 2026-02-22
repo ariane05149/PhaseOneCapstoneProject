c9121075950edccea8b11f1b0ee1470fff5867ec
ariane05149 <arianeabizera6@gmail.com>
2026-02-22 16:19:41 +0200

    added enrollCourse and assignGrade methods in the Student class

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,9 +1,12 @@
 package model;
+import java.util.Map;
+import java.util.HashMap;
 
-public class Student extends Person{
+public class Student extends Person {
     private String studentID;
     private double gpa;
     private String department;
+    private Map<Course, Double> courseGrades = new HashMap<>();
 
     public Student(String name, String id, String studentID, double gpa, String department) {
         super(name, id);
@@ -12,8 +15,21 @@ public class Student extends Person{
         this.department = department;
     }
 
+    public void enrollCourse(Course course) {
+        courseGrades.put(course, 0.0);
+    }
+
+    public void assignGrade(Course course, double grade) {
+        if (courseGrades.containsKey(course)) {
+            courseGrades.put(course, grade);
+        }
+    }
+
     @Override
     public void displayInfo() {
-        System.out.println("Student: "+getName());
+        System.out.println("Student Name: " + getName());
+        System.out.println("Student Id: " + studentID);
+        System.out.println("Department: " + department);
+        System.out.println("GPA: " + gpa);
     }
 }
