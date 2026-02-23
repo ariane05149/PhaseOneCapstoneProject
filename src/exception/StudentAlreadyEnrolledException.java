ca4ac6b62e8ddc0474955b26627bc628f851aed4
ariane05149 <arianeabizera6@gmail.com>
2026-02-23 13:29:50 +0200

    implemented the exception to be used in business logic

---
+++ src/exception/StudentAlreadyEnrolledException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class StudentAlreadyEnrolledException extends Exception{
+    public StudentAlreadyEnrolledException(String message) {
+        super(message);
+    }
+}
